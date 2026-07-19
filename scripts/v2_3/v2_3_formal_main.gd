ef76e48fdd044dc8e40d415a31f56517a0aac3d8
ryouma <junjieduan8@gmail.com>
2026-07-19 08:39:19 -0700

    feat: use player-controlled formal simulation

--- scripts/v2_3/v2_3_formal_main.gd
+++ scripts/v2_3/v2_3_formal_main.gd
@@ -1,17 +1,19 @@
 class_name V23FormalMain
 extends V23LifeLoopMain
-## Product entry retaining the formal map while adding migrated formal services.
+## Product entry retaining the formal map while making player commands authoritative.
 
 
 func _create_life_simulation() -> V2LifeLoopSimulationPolish:
-	return V23FormalSimulation.new()
+	return V23ControlledSimulation.new()
 
 
 func _create_life_binding(
 	target_simulation: V2LifeLoopSimulationPolish,
 	enable_developer_mode: bool
 ) -> V2LifeLoopUiBindingPolish:
-	return V23FormalUiBinding.new(target_simulation, enable_developer_mode)
+	return V23ControlledUiBinding.new(
+		target_simulation, enable_developer_mode
+	)
 
 
 func get_window_title() -> String:
