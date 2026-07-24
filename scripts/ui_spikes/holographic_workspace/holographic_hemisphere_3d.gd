fc9c2d7686e6f3fded1e25c73e45b4ebde3e6853
ryouma <junjieduan8@gmail.com>
2026-07-24 03:21:01 -0700

    fix: correct hemisphere mesh orientation

--- scripts/ui_spikes/holographic_workspace/holographic_hemisphere_3d.gd
+++ scripts/ui_spikes/holographic_workspace/holographic_hemisphere_3d.gd
@@ -5,8 +5,7 @@ const LON_SEGMENTS := 64
 const RADIUS := 1.0
 
 @onready var _surface: MeshInstance3D = $Surface
-var _angle: float = 0.0
-var _tilt: float = -0.24
+
 
 func _ready() -> void:
 	_surface.mesh = _build_front_hemisphere_mesh()
@@ -14,21 +13,14 @@ func _ready() -> void:
 	var material := ShaderMaterial.new()
 	material.shader = shader
 	_surface.material_override = material
-	_apply_rotation()
 
-func set_orbit(angle: float, tilt: float) -> void:
-	_angle = angle
-	_tilt = tilt
-	_apply_rotation()
-
-func _apply_rotation() -> void:
-	rotation = Vector3(_tilt, _angle, 0.0)
 
 func _build_front_hemisphere_mesh() -> ArrayMesh:
 	var vertices := PackedVector3Array()
 	var normals := PackedVector3Array()
 	var uvs := PackedVector2Array()
 	var indices := PackedInt32Array()
+
 	for lat_index in range(LAT_SEGMENTS + 1):
 		var lat_ratio := float(lat_index) / float(LAT_SEGMENTS)
 		var lat := lerpf(-PI * 0.5, PI * 0.5, lat_ratio)
@@ -43,19 +35,23 @@ func _build_front_hemisphere_mesh() -> ArrayMesh:
 			vertices.append(point)
 			normals.append(point.normalized())
 			uvs.append(Vector2(lon_ratio, 1.0 - lat_ratio))
+
 	for lat_index in range(LAT_SEGMENTS):
 		for lon_index in range(LON_SEGMENTS):
 			var a := lat_index * (LON_SEGMENTS + 1) + lon_index
 			var b := a + 1
 			var c := a + (LON_SEGMENTS + 1)
 			var d := c + 1
-			indices.append_array([a, c, b, b, c, d])
+			# Outward-facing winding for the camera on +Z looking toward the origin.
+			indices.append_array([a, b, c, b, d, c])
+
 	var arrays := []
 	arrays.resize(Mesh.ARRAY_MAX)
 	arrays[Mesh.ARRAY_VERTEX] = vertices
 	arrays[Mesh.ARRAY_NORMAL] = normals
 	arrays[Mesh.ARRAY_TEX_UV] = uvs
 	arrays[Mesh.ARRAY_INDEX] = indices
+
 	var mesh := ArrayMesh.new()
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
 	return mesh
